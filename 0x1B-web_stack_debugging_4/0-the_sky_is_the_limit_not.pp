d5f6ce18626a55c6148cdbaf39fe3a3064cec85a
khadira1937 <okhadira@.com>
2023-11-16 11:46:06 -0800

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
-#This script increases number of worker_processes
-exec { 'increase nginx worker processes':
-  command => "sed -i 's/worker_processes 5;/worker_processes 8;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-  }
+# fix our stack so that there is not failed requests
+
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
+}
